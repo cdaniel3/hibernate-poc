616eb2c75c88512b9646b875d1a80a0ec99ff0e7
Cory Daniel <9260712+cdaniel3@users.noreply.github.com>
2018-03-11 12:44:19 -0500

    initial commit

---
+++ src/main/java/tech/corydaniel/hibernatepoc/hibernatepoc/HibernatePocApplication.java
@@ -0,0 +1,12 @@
+package tech.corydaniel.hibernatepoc.hibernatepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatePocApplication.class, args);
+	}
+}
